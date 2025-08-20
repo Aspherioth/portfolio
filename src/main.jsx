8289bb85781069c1e4f7c5b77de663491b8109bf
Aspherioth <juanmpmacedo@gmail.com>
2025-08-20 11:13:31 +0100

    Removed commented theme code. Changed colors to oklch. Added routing with React Router and included a /theme route to display the current theme. Adjusted the page folds to make the content less wide.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import "./theme.css";
 import './index.css';
 import App from './App.jsx';
+import ThemePreview from "./pages/ThemePreview.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="portfolio">
+          <Route index element={<App />} />
+          <Route path="theme" element={<ThemePreview />} />
+        </Route>
+      </Routes>
+    </Router>
   </StrictMode>,
 )
