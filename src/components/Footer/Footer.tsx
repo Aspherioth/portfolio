59c11c88549bbf51bbb599f5f8531309b154500f
Aspherioth <5093062+Aspherioth@users.noreply.github.com>
2026-07-09 11:59:55 +0100

    Update footer content
    - Update copyright
    - Remove outdated Next.JS note

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,23 +1,35 @@
-import { Box, type BoxProps, Icon, Link, Text } from '@chakra-ui/react'
-import { CgExternal } from 'react-icons/cg'
+import { Box, type BoxProps, HStack, Icon, Link, Text } from "@chakra-ui/react";
+import { CgExternal } from "react-icons/cg";
 
-export default function Footer(props: React.PropsWithChildren & BoxProps): React.ReactElement {
+export default function Footer(
+  props: React.PropsWithChildren & BoxProps,
+): React.ReactElement {
   return (
     <FooterUI {...props}>
-      {props.children != null
-        ? props.children
-        : (
-          <>
-            <Text>Built with React (+ Next.js Coming Soon) | © 2025 Juan Macedo</Text>
-            <Text mt='0.5rem' textStyle='sm'>
-              <Link target='_blank' href='https://github.com/Aspherioth/portfolio'>View Source Code <Icon size='md' ><CgExternal /></Icon></Link>
-            </Text>
-          </>
-        )
+      {props.children != null ? (
+        props.children
+      ) : (
+        <>
+          <HStack justifyContent="space-between">
+            <Text>Juan Macedo</Text>
+            <Text>© 2026 · Designed & built between commits</Text>
+          </HStack>
 
-      }
+          <Text mt="0.5rem" textStyle="sm">
+            <Link
+              target="_blank"
+              href="https://github.com/Aspherioth/portfolio"
+            >
+              View Source Code{" "}
+              <Icon size="md">
+                <CgExternal />
+              </Icon>
+            </Link>
+          </Text>
+        </>
+      )}
     </FooterUI>
-  )
+  );
 }
 
 /**
@@ -25,22 +37,25 @@ export default function Footer(props: React.PropsWithChildren & BoxProps): React
  * @param children Content to be displayed within the footer.
  * @returns
  */
-function FooterUI({ children, ...props }: React.PropsWithChildren & BoxProps): React.ReactElement {
+function FooterUI({
+  children,
+  ...props
+}: React.PropsWithChildren & BoxProps): React.ReactElement {
   return (
     <Box
-      background='rgba(255, 255, 255, 0.1)'
-      backdropFilter='blur(10px)'
-      border='1px solid rgba(255, 255, 255, 0.2)'
-      textAlign='center'
-      padding='2rem'
-      margin='0 auto'
-      maxW='1200px'
-      borderRadius='15px'
-      color='text.primary'
-      w='90vw'
+      background="rgba(255, 255, 255, 0.1)"
+      backdropFilter="blur(10px)"
+      border="1px solid rgba(255, 255, 255, 0.2)"
+      textAlign="center"
+      padding="2rem"
+      margin="0 auto"
+      maxW="1200px"
+      borderRadius="15px"
+      color="text.primary"
+      w="90vw"
       {...props}
     >
       {children}
     </Box>
-  )
+  );
 }
