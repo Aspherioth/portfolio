8f26b1647cebe451a4c6b0bdd502ca610c7da6d3
Aspherioth <5093062+Aspherioth@users.noreply.github.com>
2026-07-16 11:02:30 +0100

    Add @assets alias
    - Add a /src/assets alias
    - Add image for eurovision project

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/portfolio',
+  base: "/portfolio",
   resolve: {
     alias: {
-      '@components/': '/src/components/*',
-      '@pages/': '/src/pages/*',
-      '@/*': '/src/*',
+      "@assets/": "/src/assets/*",
+      "@components/": "/src/components/*",
+      "@pages/": "/src/pages/*",
+      "@/*": "/src/*",
     },
   },
-})
+});
